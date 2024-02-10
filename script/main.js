2027b763f6327e9741ee31342c5ab27cac1bcf14
Ilia Shved <ilia83ivanovich@gmail.com>
2024-02-10 20:30:26 +0500

    final

--- script/main.js
+++ script/main.js
@@ -2,7 +2,25 @@ const swiper = new Swiper('.channel-slider', {
   // Optional parameters
   //   direction: 'vertical',
   loop: true,
-  slidesPerView: 5,
+  slidesPerView: 1,
+  spaceBetween: 20,
+  breakpoints: {
+    1900: {
+      slidesPerView: 6,
+    },
+    1400: {
+      slidesPerView: 5,
+    },
+    1200: {
+      slidesPerView: 4,
+    },
+    992: {
+      slidesPerView: 3,
+    },
+    800: {
+      slidesPerView: 2,
+    },
+  },
   // If we need pagination
   //   pagination: {
   //     el: '.swiper-pagination',
@@ -19,3 +37,86 @@ const swiper = new Swiper('.channel-slider', {
   //     el: '.swiper-scrollbar',
   //   },
 });
+
+const recommendedSlider = new Swiper('.recommended-slider', {
+  // Optional parameters
+  //   direction: 'vertical',
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 20,
+  breakpoints: {
+    1900: {
+      slidesPerView: 3,
+    },
+    1350: {
+      slidesPerView: 2,
+    },
+  },
+  // If we need pagination
+  //   pagination: {
+  //     el: '.swiper-pagination',
+  //   },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.recommended-button-next',
+    prevEl: '.recommended-button-prev',
+  },
+
+  // And if we need scrollbar
+  //   scrollbar: {
+  //     el: '.swiper-scrollbar',
+  //   },
+});
+
+const myRecommendedChannel = new Swiper('.recommendedChannel-slider', {
+  // Optional parameters
+  //   direction: 'vertical',
+  loop: true,
+  slidesPerView: 5,
+  spaceBetween: 20,
+  breakpoints: {
+    1900: {
+      slidesPerView: 6,
+    },
+    1400: {
+      slidesPerView: 5,
+    },
+    1200: {
+      slidesPerView: 4,
+    },
+    992: {
+      slidesPerView: 3,
+    },
+    800: {
+      slidesPerView: 2,
+    },
+  },
+  // If we need pagination
+  //   pagination: {
+  //     el: '.swiper-pagination',
+  //   },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.myRecommendedChannel-button-next',
+    prevEl: '.myRecommendedChannel-button-prev',
+  },
+
+  // And if we need scrollbar
+  //   scrollbar: {
+  //     el: '.swiper-scrollbar',
+  //   },
+});
+
+const searchBtn = document.querySelector('.mobile-search');
+const searchGroup = document.querySelector('.search-group');
+searchBtn.addEventListener('click', () => {
+  searchGroup.classList.toggle('is-open');
+});
+
+if (document.documentElement.scrollWidth <= 640) {
+  myRecommendedChannel.destroy();
+  recommendedSlider.destroy();
+  swiper.destroy();
+}
